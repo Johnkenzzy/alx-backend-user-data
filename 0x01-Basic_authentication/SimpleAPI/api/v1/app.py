5ad01c8229fec4df63cd3704d33820bdb16b89b0
Johnkenzzy <johnkenumeh@gmail.com>
2025-05-28 04:40:37 +0100

    Pycodestyle compliant

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -2,11 +2,13 @@
 """
 Route module for the API
 """
+
+import os
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
@@ -14,7 +16,6 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-from api.v1.auth.auth import Auth
 auth = Auht()
 
 
