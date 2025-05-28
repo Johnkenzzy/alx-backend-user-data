16df2e20b7acac7c69f37a3b465e32193a2099ed
Johnkenzzy <johnkenumeh@gmail.com>
2025-05-28 05:25:24 +0100

    Initiate Basic Authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,7 +8,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
@@ -16,7 +15,15 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-auth = Auth()
+AUTH_TYPE = getenv("AUTH_TYPE")
+
+if AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
 EXCLUDED_PATHS = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Basic Auth module
+"""
+
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic Authentication
+    """
+    pass
