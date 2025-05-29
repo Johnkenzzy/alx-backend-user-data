7a6021ab091ead518ac5bc297aa025fbd21c2fd2
Johnkenzzy <johnkenumeh@gmail.com>
2025-05-29 19:35:38 +0100

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,7 +30,8 @@ else:
 EXCLUDED_PATHS = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
 
 
@@ -43,6 +44,8 @@ def before_request_handler():
     if not auth.require_auth(request.path, EXCLUDED_PATHS):
         return
     if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
