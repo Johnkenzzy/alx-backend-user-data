14f263994defa54408f705973c1c02e25610fac6
Johnkenzzy <johnkenumeh@gmail.com>
2025-05-30 04:46:40 +0100

    Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -41,3 +41,15 @@ def session_login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route(
+        '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def session_logout():
+    """Logs out a user and destroys the session"""
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
