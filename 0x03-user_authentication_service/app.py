eaf4429d3c02d07d56d1d3ccd23709fcd3327f7e
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-05 06:26:00 +0100

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,9 +4,13 @@ Route module for the API
 """
 
 from flask import Flask, jsonify, abort, request
+from typing import Optional
 
+from auth import Auth
+from user import User
 
 app: Flask = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -14,6 +18,21 @@ def index():
     """App index view
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def user():
+    """Register a new user
+    """
+    email: Optional[str] = request.form.get('email')
+    password: Optional[str] = request.form.get('password')
+    try:
+        user: User = AUTH.register_user(email, password)
+        return jsonify(
+            {"email": email, "message": "user created"}
+        )
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
