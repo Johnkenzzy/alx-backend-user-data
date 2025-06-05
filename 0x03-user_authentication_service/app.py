0ab4fa96c500c8e9ff2def763eeaa1d325234966
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-05 12:09:13 +0100

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,10 @@
 Route module for the API
 """
 
-from flask import Flask, jsonify, abort, request, make_response, Response
+from flask import (
+    Flask, jsonify, abort, request,
+    make_response, Response, redirect, url_for
+)
 from typing import Optional
 
 from auth import Auth
@@ -51,6 +54,18 @@ def login() -> Response:
         resp.set_cookie('session_id', session_id)
         return resp
     abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> Response:
+    """Logout user
+    """
+    session_id: str = request.cookies.get('session_id')
+    user: User = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect(url_for('/'))
+    abort(403)
 
 
 if __name__ == "__main__":
