1860a27b8ecb5cf50b487b366cc1b3f671bda446
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-05 05:27:25 +0100

    Handle Error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 """Auth Module
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
 
 from db import DB
 from user import User
@@ -23,8 +24,10 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """Registers a user
         """
-        if self._db.find_user_by(email=email):
-            raise ValueError(f'user {email} already exists.')
-        hashedpw: bytes = _hash_password(password)
-        user: User = self._db.add_user(email, hashedpw)
-        return user
+        try:
+            if self._db.find_user_by(email=email):
+                raise ValueError(f'user {email} already exists.')
+        except NoResultFound:
+            hashedpw: bytes = _hash_password(password)
+            user: User = self._db.add_user(email, hashedpw)
+            return user
